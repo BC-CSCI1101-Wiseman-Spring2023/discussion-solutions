d8751bc0959179dce8d7a6b5658cc849a826cbc3
Charlie Wiseman <charles.wiseman@bc.edu>
2023-03-30 08:58:34 -0400

    adding week 10 solutions

---
+++ d10p1.py
@@ -0,0 +1,38 @@
+def lists2dict(keys, values):
+    merged = {}
+    for index,key in enumerate(keys):
+        if index >= len(values):
+            merged[key] = None
+        else:
+            merged[key] = values[index]
+    return merged
+
+letters = ["A", "B", "C"]
+phonetics = ["alpha", "bravo", "charlie"]
+military = lists2dict(letters, phonetics)
+print(military)
+
+letters = ["A", "B", "C", "D"]
+phonetics = ["alpha", "bravo", "charlie"]
+military = lists2dict(letters, phonetics)
+print(military)
+
+letters = ["A", "B", "C"]
+phonetics = ["alpha", "bravo", "charlie", "delta"]
+military = lists2dict(letters, phonetics)
+print(military)
+
+letters = ["A", "B", "C"]
+phonetics = ["alpha", "bravo", "charlie"]
+military = dict(zip(letters, phonetics))
+print(military)
+
+letters = ["A", "B", "C", "D"]
+phonetics = ["alpha", "bravo", "charlie"]
+military = dict(zip(letters, phonetics))
+print(military)
+
+letters = ["A", "B", "C"]
+phonetics = ["alpha", "bravo", "charlie", "delta"]
+military = dict(zip(letters, phonetics))
+print(military)
